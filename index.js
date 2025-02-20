6a5c00519355cfef0e08668743ac6018bf4fa6e7
SGRAVANI <shakuntla.ravani@gmail.com>
2025-02-21 00:27:55 +0530

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","appointment-frontend-chi.vercel.app"],
     credentials:true,
     
 }))
